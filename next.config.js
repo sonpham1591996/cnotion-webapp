1ba5f9b0bfb5338397832f83367ae3640c5c703d
phamminhson <39955902+minhsonfetel1996@users.noreply.github.com>
2022-08-12 23:30:30 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,30 @@
-const withPlugins = require("next-compose-plugins");
+const path = require("path");
 
-const plugins = [
-  [
-    {
-      webpack5: true,
-      reactStrictMode: true,
-    },
-  ],
-];
+module.exports = {
+  env: {
+    PASSWORD_PROTECT: process.env.ENVIRONMENT === "production",
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    externalDir: true,
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true, svgo: false } }],
+    });
 
-const nextConfig = {
-  distDir: "build",
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      next: path.resolve("./node_modules/next"),
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    };
+
     return config;
   },
-  env: {
-    PASSWORD_PROTECT: process.env.NODE_ENV === "production",
-  },
 };
-module.exports = withPlugins(plugins, nextConfig);
