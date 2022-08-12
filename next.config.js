b2eaa7ece71adb4e8534acadb51d1dff7499fa75
phamminhson <39955902+minhsonfetel1996@users.noreply.github.com>
2022-08-12 22:37:45 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,35 @@
+const withPlugins = require("next-compose-plugins");
+
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({
+const plugins = [
+  [
+    {
+      webpack5: true,
+      reactStrictMode: true,
+    },
+  ],
+];
+
+const nextConfig = {
+  distDir: "build",
   eslint: {
     dirs: ["."],
   },
   poweredByHeader: false,
   trailingSlash: true,
   basePath: "",
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
-  env: {
-    PASSWORD_PROTECT: process.env.NODE_ENV === 'production',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
   },
-});
+  env: {
+    PASSWORD_PROTECT: process.env.NODE_ENV === "production",
+  },
+};
+module.exports = withBundleAnalyzer(withPlugins(plugins, nextConfig));
